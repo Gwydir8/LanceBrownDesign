902e189ecbd667bf04e4b72d6c4b221120dfb304
Sam Gwydir <gwydir8@gmail.com>
2011-03-29 09:27:04 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LanceBrownDesign::Application.config.secret_token = '5b7139f84cfee308bd8e844cfaff123722158cd1c482cfdccf626f2e9604c59d40ec66f46114980963bb3f5b45bdc7b6df2089ac1dac23ddb7a004bb0f2f69c2'
